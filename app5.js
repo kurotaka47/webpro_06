f014cc93b61afc131d3ac3bf1101593b2852c112
kurotaka47 <s24g1054dz@s.chibakoudai.jp>
2024-11-18 22:11:36 +0900

    app5.js

--- app5.js
+++ app5.js
@@ -77,4 +77,73 @@ app.get("/janken", (req, res) => {
   res.render('janken', display);
 });
 
+// Atti Muite Hoi
+app.get("/atti", (req, res) => {
+  let hand = req.query.hand;
+  let win = Number(req.query.win);
+  let total = Number(req.query.total);
+  console.log({ hand, win, total });
+  const num = Math.floor(Math.random() * 4 + 1);
+  let cpu = '';
+  if (num == 1) cpu = '上';
+  else if (num == 2) cpu = '下';
+  else if (num == 3) cpu = '左';
+  else cpu = '右';
+
+  // 勝敗の判定
+  let judgement = '';
+  if (hand === cpu) {
+    judgement = '負け';
+  } else {
+    judgement = '勝ち';
+    win += 1;
+  }
+
+  total += 1;
+
+  const display = {
+    your: hand,
+    cpu: cpu,
+    judgement: judgement,
+    win: win,
+    total: total
+  }
+  res.render('atti', display);
+});
+
+// Yubi Suma
+app.get("/yubi", (req, res) => {
+  let hand = req.query.hand;
+  let win = Number(req.query.win);
+  let total = Number(req.query.total);
+  console.log({ hand, win, total });
+  const num = Math.floor(Math.random() * 5 + 1);
+  let cpu = '';
+  if (num == 1) cpu = '0';
+  else if (num == 2) cpu = '1';
+  else if (num == 3) cpu = '2';
+  else if (num == 4) cpu = '3';
+  else cpu = '4';
+  
+  // 勝敗の判定
+  let judgement = '';
+  if (hand === cpu) {
+    judgement = '負け';
+  } else {
+    judgement = '勝ち';
+    win += 1;
+  }
+
+  total += 1;
+
+  const display = {
+    your: hand,
+    cpu: cpu,
+    judgement: judgement,
+    win: win,
+    total: total
+  }
+  res.render('yubi', display);
+});
+
 app.listen(8080, () => console.log("Example app listening on port 8080!"));
