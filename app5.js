f6540245ceba52e83c44f46732f48ea2b7c9d9e2
kurotaka47 <s24g1054dz@s.chibakoudai.jp>
2024-11-18 20:51:49 +0900

    app5.js

--- app5.js
+++ app5.js
@@ -77,4 +77,140 @@ app.get("/janken", (req, res) => {
   res.render('janken', display);
 });
 
+app.get("/atti", (req, res) => {
+  let hand = req.query.hand;
+  let win = Number(req.query.win);
+  let total = Number(req.query.total);
+  console.log({ hand, win, total });
+  const num = Math.floor(Math.random() * 4 + 1);
+  let cpu = '';
+  if (num == 1) cpu = '上';
+  else if (num == 2) cpu = '下';
+  else if (num == 3) cpu = '右';
+  else cpu = '左';
+
+  // 勝敗の判定
+  let judgement = '';
+  if (hand === '上') {
+    if (cpu === '上') judgement = '負け';
+    else if (cpu === '下') {
+      judgement = '勝ち';
+      win += 1;
+    } else judgement = '勝ち';
+
+
+  } else if (hand === '下') {
+    if (cpu === '下') judgement = '負け';
+    else if (cpu === '上') judgement = '勝ち';
+    else {
+      judgement = '勝ち';
+      win += 1;
+    }
+
+
+  } else if (hand === '右') {
+    if (cpu === '右') {
+      judgement = '負け';
+      win += 1;
+    } else if (cpu === '上') judgement = '勝ち';
+    else judgement = '勝ち';
+  } else {
+    judgement = '勝ち';
+  }
+
+
+ } else if (hand === '左') {
+    if (cpu === '左') {
+      judgement = '負け';
+      win += 1;
+    } else if (cpu === '上') judgement = '勝ち';
+    else judgement = '勝ち';
+  } else {
+    judgement = '勝ち';
+  }
+
+  if (judgement !== '勝ち') {
+    total += 1;
+  }
+
+
+app.get("/yubisuma", (req, res) => {
+  let hand = req.query.hand;
+  let win = Number(req.query.win);
+  let total = Number(req.query.total);
+  console.log({ hand, win, total });
+  const num = Math.floor(Math.random() * 5 + 1);
+  let cpu = '';
+  if (num == 1) cpu = '0';
+  else if (num == 2) cpu = '1';
+  else if (num == 3) cpu = '2';
+  else if (num == 4) cpu = '3';
+  else cpu = '4';
+
+  // 勝敗の判定
+  let judgement = '';
+  if (hand === '0') {
+    if (cpu === '0') judgement = '負け';
+    else if (cpu === '1') {
+      judgement = '勝ち';
+      win += 1;
+    } else judgement = '勝ち';
+
+
+  } else if (hand === '1') {
+    if (cpu === '1') judgement = '負け';
+    else if (cpu === '0') judgement = '勝ち';
+    else {
+      judgement = '勝ち';
+      win += 1;
+    }
+
+
+  } else if (hand === '2') {
+    if (cpu === '2') {
+      judgement = '負け';
+      win += 1;
+    } else if (cpu === '0') judgement = '勝ち';
+    else judgement = '勝ち';
+  } else {
+    judgement = '勝ち';
+  }
+
+
+ } else if (hand === '3') {
+    if (cpu === '3') {
+      judgement = '負け';
+      win += 1;
+    } else if (cpu === '0') judgement = '勝ち';
+    else judgement = '勝ち';
+  } else {
+    judgement = '勝ち';
+  }
+
+ } else if (hand === '4') {
+    if (cpu === '4') {
+      judgement = '負け';
+      win += 1;
+    } else if (cpu === '0') judgement = '勝ち';
+    else judgement = '勝ち';
+  } else {
+    judgement = '勝ち';
+  }
+
+  if (judgement !== '勝ち') {
+    total += 1;
+  }
+
+
+
+  const display = {
+    your: hand,
+    cpu: cpu,
+    judgement: judgement,
+    win: win,
+    total: total
+  }
+  res.render('janken', display);
+});
+
 app.listen(8080, () => console.log("Example app listening on port 8080!"));
